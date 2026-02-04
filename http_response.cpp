44de79d39d5deff3e280b2e75548043914c82f08
Justin Ferguson <jf@ownco.net>
2026-02-04 11:54:36 -0800

    Update http_response.cpp
    There was some sort of bug in the original upload of the HTTP client code and I'm not sure what it was, but it wasn't in the repo that actually made use of the code, so I extracted it all and made sure it worked and recommitted it all to github.

--- http_response.cpp
+++ http_response.cpp
@@ -2,7 +2,8 @@
 
 http_response_t::http_response_t(http_version_t version, std::size_t status, std::string text, header_map_t headers, std::string body)
 	: m_version(version), m_status(status), m_status_text(text), m_headers(headers), m_body(body)
-{}
+{
+}
 
 const http_version_t
 http_response_t::version(void) const
@@ -17,41 +18,41 @@ http_response_t::version(const http_version_t v)
 	return;
 }
 
-const std::size_t 
-http_response_t::status(void) const 
-{ 
-	return m_status; 
+const std::size_t
+http_response_t::status(void) const
+{
+	return m_status;
 }
 
-void 
-http_response_t::status(const std::size_t status) 
-{ 
-	m_status = status; 
-	return; 
+void
+http_response_t::status(const std::size_t status)
+{
+	m_status = status;
+	return;
 }
 
-std::string 
+std::string
 http_response_t::status_text(void) const
-{ 
-	return m_status_text; 
+{
+	return m_status_text;
 }
 
-void 
-http_response_t::status_text(const std::string text) 
-{ 
-	m_status_text = text; 
-	return; 
+void
+http_response_t::status_text(const std::string text)
+{
+	m_status_text = text;
+	return;
 }
 
-void 
+void
 http_response_t::add_header(const std::string name, const std::string value)
 {
 	m_headers[name] = value;
 	return;
 }
 
-std::string 
-http_response_t::get_header(const std::string name) 
+std::string
+http_response_t::get_header(const std::string name)
 {
 	if (false == this->has_header(name))
 		return std::string("");
@@ -59,28 +60,28 @@ http_response_t::get_header(const std::string name)
 	return m_headers[name];
 }
 
-bool 
+bool
 http_response_t::has_header(const std::string name) const
 {
 	return m_headers.find(name) != m_headers.end();
 }
 
-std::string 
-http_response_t::body(void) 
-{ 
-	return m_body; 
+std::string
+http_response_t::body(void)
+{
+	return m_body;
 }
 
-void 
-http_response_t::body(const std::string body) 
-{ 
-	m_body = body; 
-	return; 
+void
+http_response_t::body(const std::string body)
+{
+	m_body = body;
+	return;
 }
 
-void 
-http_response_t::append_body(const std::string body) 
-{ 
-	m_body += body; 
-	return; 
+void
+http_response_t::append_body(const std::string body)
+{
+	m_body += body;
+	return;
 }
